5bf09040e8d4a0c3ec8037888b69548e8ec6ca32
polina-g <ignatieva.polina@gmail.com>
2021-09-20 10:00:44 -0400

    creatures categiry displays subcategory first, then item list - fixed

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ let categoryData;
 
 //CACHE ELEMENTS
 const $container = $('#container');
+const $listContainer = $('#list-container')
 const $list = $('ul');
 
+$listContainer.hide();
 $container.on('click', showData)
 
+//Monitoring API requests//
 $(document).ajaxStart(function() {
     console.log('request started')
 })
@@ -18,20 +21,29 @@ $(document).ajaxStop(function() {
     console.log('request complete')
 })
 
+//Showing API data when category container clicked
 function showData(event) {
     let category = $(event.target).attr('id');
+    $listContainer.fadeIn();
+    
     $.ajax(`${BASE_URL}/category/${category}`).then(function(data) {
-        categoryData = data.data;
+        //Remove list items from previous API call
         $('.list-item').remove();
+        categoryData = data.data; 
 
         if (category === 'creatures') {
-            $list.append(`<li class="subcategory" id="food">Food</li><li id="non_food">Not Food</li>`)
+            $list.append(`<li class="subcategory" id="food">Food</li><li class="subcategory" id="non_food">Animals</li>`)
             $('li').click(function(event) {
                 subCategory = $(event.target).attr('id');
-            })
+                $('.subcategory').remove();
+                categoryData = data.data[subCategory]
+                console.log(categoryData);
+                renderList();
+            })        
         }
 
         renderList();
+
     }, function() {
         console.log('Something went wrong')
     })
@@ -39,15 +51,15 @@ function showData(event) {
 }
 
 function renderList() {
-    if (category === 'creatures') {
-        console.log(subCategory)
-        $('.subcategory').remove()
-        categoryData = categoryData.subcategory;
-        for (let i = 0; i < categoryData.length; i++) {
-            $list.append(`<li class="list-item">${categoryData[i].name}</li>`)
-        }
+    // if (category === 'creatures') {
+    //     console.log(subCategory)
+    //     $('.subcategory').remove()
+    //     categoryData = categoryData.subcategory;
+    //     for (let i = 0; i < categoryData.length; i++) {
+    //         $list.append(`<li class="list-item">${categoryData[i].name}</li>`)
+    //     }
         
-    }
+    // }
 
     for (let i = 0; i < categoryData.length; i++) {
         $list.append(`<li class="list-item">${categoryData[i].name}</li>`)
